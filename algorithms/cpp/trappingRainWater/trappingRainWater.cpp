8a35254e761885bf4669c93774efe86e367e7a7c
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-09 08:07:05 +0900

    Update trappingRainWater.cpp

--- algorithms/cpp/trappingRainWater/trappingRainWater.cpp
+++ algorithms/cpp/trappingRainWater/trappingRainWater.cpp
@@ -1,83 +1,60 @@
-// Source : https://oj.leetcode.com/problems/trapping-rain-water/
-// Author : Hao Chen
-// Date   : 2014-07-02
-
-/********************************************************************************** 
-* 
-* Given n non-negative integers representing an elevation map where the width of each bar is 1, 
-* compute how much water it is able to trap after raining. 
-* 
-* For example, 
-* Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
-*   
-*     ^                                             
-*     |                                             
-*   3 |                       +--+                  
-*     |                       |  |                  
-*   2 |          +--+xxxxxxxxx|  +--+xx+--+         
-*     |          |  |xxxxxxxxx|  |  |xx|  |         
-*   1 |   +--+xxx|  +--+xxx+--+  |  +--+  +--+      
-*     |   |  |xxx|  |  |xxx|  |  |  |  |  |  |      
-*   0 +---+--+---+--+--+---+--+--+--+--+--+--+----->
-*       0  1   0  2  1   0  1  3  2  1  2  1        
-* 
-* The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 
-* 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
-*               
-**********************************************************************************/
-
-#include <stdio.h>
-/*
- * The idea is:
- *    1) find the highest bar.
- *    2) traverse the bar from left the highest bar.
- *       becasue we have the highest bar in right, so, any bar higher than its right bar(s) can contain the water.
- *    3) traverse the bar from right the highest bar.
- *       becasue we have the highest bar in left, so, any bar higher than its left bar(s) can contain the water.
- *
- * The code below is quite clear!
- *
- */
-int trap(int a[], int n) {
-    int result = 0;
-
-    //find the highest value/position
-    int maxHigh = 0;
-    int maxIdx = 0;
-    for(int i=0; i<n; i++){
-        if (a[i] > maxHigh){
-            maxHigh = a[i];
-            maxIdx = i;
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res = 0, mx = 0, n = height.size();
+        vector<int> dp(n, 0);
+        for (int i = 0; i < n; ++i) {
+            dp[i] = mx;
+            mx = max(mx, height[i]);
         }
-    }
-
-    //from the left to the highest postion
-    int prevHigh = 0;
-    for(int i=0; i<maxIdx; i++){
-        if(a[i] > prevHigh){
-            prevHigh = a[i];
+        mx = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = min(dp[i], mx);
+            mx = max(mx, height[i]);
+            if (dp[i] > height[i]) res += dp[i] - height[i];
         }
-        result += (prevHigh - a[i]);
+        return res;
     }
+};
 
-    //from the right to the highest postion
-    prevHigh=0;
-    for(int i=n-1; i>maxIdx; i--){
-        if(a[i] > prevHigh){
-            prevHigh = a[i];
-        }
-        result += (prevHigh - a[i]);
-    }
 
-    return result;
-}
+// class Solution {
+// public:
+//   int trap(vector<int>& height) {
+//     const int n = height.size();
+//     vector<int> l(n);
+//     vector<int> r(n);
+//     int ans = 0;
+//     for (int i = 0; i < n; ++i)
+//       l[i] = i == 0 ? height[i] : max(l[i - 1], height[i]);
+//     for (int i = n - 1; i >= 0; --i)
+//       r[i] = i == n - 1 ? height[i] : max(r[i + 1], height[i]);
+//     for (int i = 0; i < n; ++i)
+//       ans += min(l[i], r[i]) - height[i];
+//     return ans;
+//   }
+// };
 
-#define TEST(a) printf("%d\n", trap(a, sizeof(a)/sizeof(int)))
-int main()
-{
-    int a[]={2,0,2};
-    TEST(a);
-    int b[]={0,1,0,2,1,0,1,3,2,1,2,1};
-    TEST(b);
-    return 0;
-}
+
+// class Solution {
+// public:
+//   int trap(vector<int>& height) {
+//     const int n = height.size();
+//     if (n == 0) return 0;
+//     int l = 0;
+//     int r = n - 1;
+//     int max_l = height[l];
+//     int max_r = height[r];
+//     int ans = 0;
+//     while (l < r) {      
+//       if (max_l < max_r) {
+//         ans += max_l - height[l];
+//         max_l = max(max_l, height[++l]);
+//       } else {
+//         ans += max_r - height[r];
+//         max_r = max(max_r, height[--r]);
+//       }
+//     }
+//     return ans;
+//   }
+// };
