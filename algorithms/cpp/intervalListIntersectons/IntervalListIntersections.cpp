9ceb909452ca4470e1eb8e27fead1dd0667cc978
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-08 11:32:45 +0900

    Update IntervalListIntersections.cpp

--- algorithms/cpp/intervalListIntersectons/IntervalListIntersections.cpp
+++ algorithms/cpp/intervalListIntersectons/IntervalListIntersections.cpp
@@ -37,58 +37,20 @@
  * 
  ******************************************************************************************************/
 
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
 public:
-    //return true if lhs starts earlier than  rhs
-    bool compareInterval(Interval& lhs, Interval& rhs) {
-        return lhs.start < rhs.start;
-    }
-    //check two interval overlapped or not
-    bool overlapped(Interval& lhs, Interval& rhs) {
-        return (compareInterval(lhs, rhs)) ?
-                             lhs.end >= rhs.start:
-                             rhs.end >= lhs.start;
-
-    }
-    //merge two interval - return the intersections of two intervals
-    Interval mergeTwoInterval(Interval& lhs, Interval& rhs) {
-        Interval result;
-        result.start = max(lhs.start, rhs.start);
-        result.end = min(lhs.end, rhs.end);
-        return result;
-    }
-
-    vector<Interval> intervalIntersection(vector<Interval>& A, vector<Interval>& B) {
-        int lenA = A.size();
-        int lenB = B.size();
-
-        vector<Interval> result;
-        if (lenA <=0 || lenB<=0) return result; //edge case
-
-        int i=0, j=0;
-        while ( i < lenA && j < lenB ) {
-            if( overlapped(A[i], B[j]) ) {
-                result.push_back(mergeTwoInterval(A[i], B[j]));
-                // if the current interval is not overlapped with next one,
-                // then we move the next interval.
-                int nexti = i;
-                if ( j==lenB-1 || !overlapped(A[i], B[j+1]) ) nexti=i+1;
-                if ( i==lenA-1 || !overlapped(A[i+1], B[j]) ) j++;
-                i = nexti;
-            }else{
-                //if not overlapped, we just move the next one
-                compareInterval(A[i], B[j]) ? i++ : j++;
-            }
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int m = A.size(), n = B.size();
+        vector<vector<int>> res;
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            auto p = A[i], q = B[j];
+            int start = std::max(p[0], q[0]);
+            int end = std::min(p[1], q[1]);
+            if (start <= end) res.push_back({start, end});
+            if (p[1] < q[1]) ++ i;
+            else ++ j;
         }
-        return result;
+        return res;
     }
 };
