0cdb70004d192e790a33492912503ddb8b3394b3
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-09 22:39:06 +0900

    解答追加

--- algorithms/cpp/lengthOfLastWord/lengthOfLastWord.cpp
+++ algorithms/cpp/lengthOfLastWord/lengthOfLastWord.cpp
@@ -1,59 +1,13 @@
-// Source : https://oj.leetcode.com/problems/length-of-last-word/
-// Author : Hao Chen
-// Date   : 2014-07-01
-
-/********************************************************************************** 
-* 
-* Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
-* return the length of last word in the string.
-* 
-* If the last word does not exist, return 0.
-* 
-* Note: A word is defined as a character sequence consists of non-space characters only.
-* 
-* For example, 
-* Given s = "Hello World",
-* return 5.
-* 
-*               
-**********************************************************************************/
-
-#include <stdio.h>
-#include <ctype.h>
-
-int lengthOfLastWord(const char *s) {
-
-    if ( !s ||!*s ) return 0;
-
-    int wordLen=0;
-    int cnt=0;
-
-    for (;*s!='\0';s++) {
-        if (isalpha(*s)){
-            cnt++;
-        }
-        if (!isalpha(*s)){
-            if (cnt>0){
-                wordLen = cnt;
+    class Solution {
+    public:
+        int lengthOfLastWord(string s) {
+            int res = 0;
+            for (int i = 0; i < s.size(); ++i) {
+                if (s[i] != ' ') {
+                    if (i != 0 && s[i - 1] == ' ') res = 1;
+                    else ++res;
+                }
             }
-            cnt=0;
+            return res;
         }
-    }
-
-    return cnt>0 ? cnt : wordLen;
-}
-
-
-int main(int argc, char** argv)
-{
-    const char* p;
-    p = "hello world";
-    printf("%s, %d\n", p, lengthOfLastWord(p)); 
-    p = "a";
-    printf("%s, %d\n", p, lengthOfLastWord(p)); 
-    
-    if(argc>1){
-        p = argv[1];
-        printf("%s, %d\n", p, lengthOfLastWord(p)); 
-    }
-}
+    };
