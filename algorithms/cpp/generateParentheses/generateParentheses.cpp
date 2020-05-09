4cd949162e2468716df1c6715dc6cd202d5f0230
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-09 21:10:08 +0900

    解答追加

--- algorithms/cpp/generateParentheses/generateParentheses.cpp
+++ algorithms/cpp/generateParentheses/generateParentheses.cpp
@@ -1,60 +1,40 @@
-// Source : https://oj.leetcode.com/problems/generate-parentheses/
-// Author : Hao Chen
-// Date   : 2014-06-29
-
-/********************************************************************************** 
-* 
-* Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-* 
-* For example, given n = 3, a solution set is:
-* 
-* "((()))", "(()())", "(())()", "()(())", "()()()"
-* 
-*               
-**********************************************************************************/
-
-#include <stdlib.h>
-#include <iostream>
-#include <vector>
-using namespace std;
-
-
-void generator(vector<string>& result, int left, int right, string s);
-vector<string> generateParenthesis(int n) {
-    
-    vector<string> result;
-    string s;
-    generator(result, n, n, s);
-    return result;
-}
-
-void generator(vector<string>& result, int left, int right, string s){
-    if (left==0 && right==0){
-        result.push_back(s);
-        return;
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        generateParenthesisDFS(n, n, "", res);
+        return res;
     }
-    if (left>0){
-        generator(result, left-1, right, s+'(');
+    void generateParenthesisDFS(int left, int right, string out, vector<string> &res) {
+        if (left > right) return;
+        if (left == 0 && right == 0) res.push_back(out);
+        else {
+            if (left > 0) generateParenthesisDFS(left - 1, right, out + '(', res);
+            if (right > 0) generateParenthesisDFS(left, right - 1, out + ')', res);
+        }
     }
-    if (right>0 && right>left){
-        generator(result, left, right-1, s+')');
-    }
-}
-    
-void printResult(vector<string>& result)
-{
-    for(int i=0; i<result.size(); i++){
-        cout << result[i] <<endl;
-    }
-}
+};
 
-int main(int argc, char** argv) 
-{
-    int n=3;
-    if (argc>1){
-        n = atoi(argv[1]);
-    }
-    vector<string> r = generateParenthesis(n);
-    printResult(r);
-    return 0;
-}
+
+//class Solution {
+//public:
+//    vector<string> generateParenthesis(int n) {
+//        unordered_set<string> st;
+//        if (n == 0) st.insert("");
+//        else {
+//            vector<string> pre = generateParenthesis(n - 1);
+//            for (auto a : pre) {
+//                for (int i = 0; i < a.size(); ++i) {
+//                    if (a[i] == '(') {
+//                        a.insert(a.begin() + i + 1, '(');
+//                        a.insert(a.begin() + i + 2, ')');
+//                        st.insert(a);
+//                        a.erase(a.begin() + i + 1, a.begin() + i + 3);
+//                    }
+//                }
+//                st.insert("()" + a);
+//            }
+//        }
+//        return vector<string>(st.begin(), st.end());
+//    }
+//};
