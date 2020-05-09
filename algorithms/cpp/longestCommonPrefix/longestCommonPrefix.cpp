6e8f844cc63e932b2e0807ee33a49d87918d9c14
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-09 20:28:55 +0900

    Update longestCommonPrefix.cpp

--- algorithms/cpp/longestCommonPrefix/longestCommonPrefix.cpp
+++ algorithms/cpp/longestCommonPrefix/longestCommonPrefix.cpp
@@ -1,44 +1,32 @@
-// Source : https://oj.leetcode.com/problems/longest-common-prefix/
-// Author : Hao Chen
-// Date   : 2014-07-03
-
-/********************************************************************************** 
-* 
-* Write a function to find the longest common prefix string amongst an array of strings.
-* 
-*               
-**********************************************************************************/
-
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
-
-
-string longestCommonPrefix(vector<string> &strs) {
-    string word;
-    if (strs.size()<=0) return word;
-    for(int i=1; i<=strs[0].size(); i++){
-        string w = strs[0].substr(0, i);
-        bool match = true;
-        int j=1;
-        for(j=1; j<strs.size(); j++){
-            if (i>strs[j].size() || w!=strs[j].substr(0, i) ) {
-                match=false;
-                break;
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string res = "";
+        for (int j = 0; j < strs[0].size(); ++j) {
+            char c = strs[0][j];
+            for (int i = 1; i < strs.size(); ++i) {
+                if (j >= strs[i].size() || strs[i][j] != c) {
+                    return res;
+                }
             }
+            res.push_back(c);
         }
-        if (!match) {
-            return word;
-        }
-        word = w;
+        return res;
     }
-    return word;
-}
+};
 
-int main()
-{
-    const char* s[]={"abab","aba","abc"};
-    vector<string> v(s, s+3);
-    cout << longestCommonPrefix(v) <<endl;
-}
+//class Solution {
+//public:
+//    string longestCommonPrefix(vector<string>& strs) {
+//        if (strs.empty()) return "";
+//        for (int j = 0; j < strs[0].size(); ++j) {
+//            for (int i = 0; i < strs.size(); ++i) {
+//                if (j >= strs[i].size() || strs[i][j] != strs[0][j]) {
+//                    return strs[i].substr(0, j);
+//                }
+//            }
+//        }
+//        return strs[0];
+//    }
+//};
