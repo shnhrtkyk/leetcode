8f3d85e23c1fe300f43a051746b4628096c205be
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-09 20:24:19 +0900

    Update romanToInteger.cpp

--- algorithms/cpp/romanToInteger/romanToInteger.cpp
+++ algorithms/cpp/romanToInteger/romanToInteger.cpp
@@ -1,68 +1,13 @@
-// Source : https://oj.leetcode.com/problems/roman-to-integer/
-// Author : Hao Chen
-// Date   : 2014-07-17
-
-/********************************************************************************** 
-* 
-* Given a roman numeral, convert it to an integer.
-* 
-* Input is guaranteed to be within the range from 1 to 3999.
-*               
-**********************************************************************************/
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-int romanCharToInt(char ch){
-    int d = 0;
-    switch(ch){
-        case 'I':  
-            d = 1;  
-            break;  
-        case 'V':  
-            d = 5;  
-            break;  
-        case 'X':  
-            d = 10;  
-            break;  
-        case 'L':  
-            d = 50;  
-            break;  
-        case 'C':  
-            d = 100;  
-            break;  
-        case 'D':  
-            d = 500;  
-            break;  
-        case 'M':  
-            d = 1000;  
-            break;  
-    }
-    return d;
-}
-int romanToInt(string s) {
-    if (s.size()<=0) return 0;
-    int result = romanCharToInt(s[0]);
-    for (int i=1; i<s.size(); i++){
-        int prev = romanCharToInt(s[i-1]);
-        int curr = romanCharToInt(s[i]);
-        //if left<right such as : IV(4), XL(40), IX(9) ...
-        if (prev < curr) {
-            result = result - prev + (curr-prev);
-        }else{
-            result += curr;
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+        unordered_map<char, int> m{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        for (int i = 0; i < s.size(); ++i) {
+            int val = m[s[i]];
+            if (i == s.size() - 1 || m[s[i+1]] <= m[s[i]]) res += val;
+            else res -= val;
         }
+        return res;
     }
-    return result;
-}
-
-int main(int argc, char**argv)
-{
-    string s("XL");
-    if (argc>1){
-        s = argv[1];
-    }
-    cout << s << " : " << romanToInt(s) << endl;
-    return 0;
-}
+};
