c4c183133cfdc247527407bff110e506a7eb26ca
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-09 22:57:40 +0900

    Update plusOne.cpp

--- algorithms/cpp/plusOne/plusOne.cpp
+++ algorithms/cpp/plusOne/plusOne.cpp
@@ -1,51 +1,15 @@
-// Source : https://oj.leetcode.com/problems/plus-one/
-// Author : Hao Chen
-// Date   : 2014-06-21
-
-/********************************************************************************** 
-* 
-* Given a non-negative number represented as an array of digits, plus one to the number.
-* 
-* The digits are stored such that the most significant digit is at the head of the list.
-*               
-**********************************************************************************/
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<int> plusOne(vector<int> &digits) {
-    int carry=1;
-    vector <int> v;
-    while(digits.size()>0){
-        int x = digits.back();
-        digits.pop_back();
-        x = x + carry;
-        v.insert(v.begin(), x%10);
-        carry = x/10;
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] == 9) digits[i] = 0;
+            else {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+        if (digits.front() == 0) digits.insert(digits.begin(), 1);
+        return digits;
     }
-    if (carry>0){
-        v.insert(v.begin(), carry);
-    }
-    return v;
-
-}
-
-void printVector(vector<int>& v)
-{
-    cout << "{ ";
-    for(int i=0; i<v.size(); i++){
-        cout << v[i] << " ";
-    }
-    cout << "} " << endl;
-}
-
-
-int main()
-{
-    int a[]={9,9,9};
-    vector<int> d(&a[0], &a[0]+sizeof(a)/sizeof(int));
-    vector<int> v = plusOne(d);
-    printVector(v);
-    return 0;
-}
+};
