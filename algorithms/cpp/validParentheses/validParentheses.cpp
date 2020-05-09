bdc9a8b19dff6fdd31cf757636d30f5a3cb89f3d
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-09 20:58:32 +0900

    Update validParentheses.cpp

--- algorithms/cpp/validParentheses/validParentheses.cpp
+++ algorithms/cpp/validParentheses/validParentheses.cpp
@@ -1,49 +1,17 @@
-// Source : https://oj.leetcode.com/problems/valid-parentheses/
-// Author : Hao Chen
-// Date   : 2014-06-30
-
-/********************************************************************************** 
-* 
-* Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
-* determine if the input string is valid.
-* 
-* The brackets must close in the correct order, "()" and "()[]{}" are all valid 
-* but "(]" and "([)]" are not.
-* 
-*               
-**********************************************************************************/
-
-#include <iostream>
-#include <string>
-#include <stack>
-using namespace std;
-
-bool isValid(string s) {
-    stack<char> st;
-    for(auto ch : s) {
-        if (ch=='{' || ch =='[' || ch=='(' ) {
-            st.push(ch);
-        }else if (ch=='}' || ch ==']' || ch == ')' ){
-            if (st.empty()) return false;
-            char sch = st.top();
-            if ( (sch=='{' && ch =='}') || (sch=='[' && ch==']') || (sch=='(' && ch==')' ) ){
-                st.pop();
-            }else {
-                return false;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> parentheses;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') parentheses.push(s[i]);
+            else {
+                if (parentheses.empty()) return false;
+                if (s[i] == ')' && parentheses.top() != '(') return false;
+                if (s[i] == ']' && parentheses.top() != '[') return false;
+                if (s[i] == '}' && parentheses.top() != '{') return false;
+                parentheses.pop();
             }
-        }else{
-            return false;
         }
+        return parentheses.empty();
     }
-    return st.empty();
-}
-
-int main(int argc, char**argv)
-{
-    string s = "{{}{[]()}}";
-    if (argc>1){
-        s = argv[1];
-    }
-    cout << "str = \"" << (s) << "\"" << endl;
-    cout << isValid(s) << endl;
-}
+}; 
