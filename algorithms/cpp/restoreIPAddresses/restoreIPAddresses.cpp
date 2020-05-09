654d748a082a199949683b463f56ee53f3a9313f
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-09 23:40:57 +0900

    Update restoreIPAddresses.cpp

--- algorithms/cpp/restoreIPAddresses/restoreIPAddresses.cpp
+++ algorithms/cpp/restoreIPAddresses/restoreIPAddresses.cpp
@@ -1,75 +1,26 @@
-// Source : https://oj.leetcode.com/problems/restore-ip-addresses/
-// Author : Hao Chen
-// Date   : 2014-08-26
-
-/********************************************************************************** 
-* 
-* Given a string containing only digits, restore it by returning all possible valid IP address combinations.
-* 
-* For example:
-* Given "25525511135",
-* 
-* return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
-* 
-*               
-**********************************************************************************/
-
-#include <stdlib.h>
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
-
-void restoreIpAddressesHelper(string& s, int start, int partNum, string ip, vector<string>& result);
-
-vector<string> restoreIpAddresses(string s) {
-    vector<string> result;
-    string ip;
-    restoreIpAddressesHelper(s, 0, 0, ip, result);
-    return result;
-}
-
-void restoreIpAddressesHelper(string& s, int start, int partNum, string ip, vector<string>& result) {
-
-    int len = s.size();
-    if ( len - start < 4-partNum  || len - start > (4-partNum)*3 ) {
-        return;
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> res;
+        restore(s, 4, "", res);
+        return res;
     }
-
-    if (partNum == 4 && start == len){
-        ip.erase(ip.end()-1, ip.end());
-        result.push_back(ip);
-        return;
-    }
-
-    int num = 0;
-    for (int i=start; i<start+3; i++){
-        num = num*10 + s[i]-'0';
-        if (num<256){
-            ip+=s[i];
-            restoreIpAddressesHelper(s, i+1, partNum+1, ip+'.', result);
-        }         
-        //0.0.0.0 valid, but 0.1.010.01 is not
-        if (num == 0) {
-            break;
+    void restore(string s, int k, string out, vector<string> &res) {
+        if (k == 0) {
+            if (s.empty()) res.push_back(out);
+        }
+        else {
+            for (int i = 1; i <= 3; ++i) {
+                if (s.size() >= i && isValid(s.substr(0, i))) {
+                    if (k == 1) restore(s.substr(i), k - 1, out + s.substr(0, i), res);
+                    else restore(s.substr(i), k - 1, out + s.substr(0, i) + ".", res);
+                }
+            }
         }
     }
-       
-}
-
-
-int main(int argc, char**argv)
-{
-    string s = "25525511135";
-    if (argc>1){
-        s = argv[1];
+    bool isValid(string s) {
+        if (s.empty() || s.size() > 3 || (s.size() > 1 && s[0] == '0')) return false;
+        int res = atoi(s.c_str());
+        return res <= 255 && res >= 0;
     }
-
-    vector<string> result = restoreIpAddresses(s);
-
-    cout << s << endl;
-    for(int i=0; i<result.size(); i++){
-        cout << '\t' << result[i] << endl;
-    }
-    return 0;
-}
+};
