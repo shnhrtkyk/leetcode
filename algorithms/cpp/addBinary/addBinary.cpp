b513f5926801e45199e04da44fe690f1c7bb5fa1
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-09 22:58:30 +0900

    Update addBinary.cpp

--- algorithms/cpp/addBinary/addBinary.cpp
+++ algorithms/cpp/addBinary/addBinary.cpp
@@ -1,52 +1,39 @@
-// Source : https://oj.leetcode.com/problems/add-binary/
-// Author : Hao Chen
-// Date   : 2014-07-05
-
-/********************************************************************************** 
-* 
-* Given two binary strings, return their sum (also a binary string).
-* 
-* For example,
-* a = "11"
-* b = "1"
-* Return "100".
-* 
-*               
-**********************************************************************************/
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-string addBinary(string a, string b) {
-    int alen = a.size();
-    int blen = b.size();
-    bool carry = false;
-    string result;
-    while( alen>0 || blen>0) {
-        int abit = alen<=0 ? 0 : a[alen-1]-'0';
-        int bbit = blen<=0 ? 0 : b[blen-1]-'0';
-        int cbit = carry ? 1 : 0;
-        result.insert(result.begin(), '0' + ((abit+bbit+cbit) & 1) );
-        carry = (abit+bbit+cbit>1);
-        alen--; blen--;
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res;
+        int na = a.size();
+        int nb = b.size();
+        int n = max(na, nb);
+        bool carry = false;
+        if (na > nb) {
+            for (int i = 0; i < na - nb; ++i) b.insert(b.begin(), '0');
+        }
+        else if (na < nb) {
+            for (int i = 0; i < nb - na; ++i) a.insert(a.begin(), '0');
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            int tmp = 0;
+            if (carry) tmp = (a[i] - '0') + (b[i] - '0') + 1;
+            else tmp = (a[i] - '0') + (b[i] - '0');
+            if (tmp == 0) {
+                res.insert(res.begin(), '0');
+                carry = false;
+            }
+            else if (tmp == 1) {
+                res.insert(res.begin(), '1');
+                carry = false;
+            }
+            else if (tmp == 2) {
+                res.insert(res.begin(), '0');
+                carry = true;
+            }
+            else if (tmp == 3) {
+                res.insert(res.begin(), '1');
+                carry = true;
+            }
+        }
+        if (carry) res.insert(res.begin(), '1');
+        return res;
     }
-    if (carry){
-        result.insert(result.begin(), '1');
-    }
-    return result;
-}
-
-
-int main(int argc, char** argv) 
-{
-    string a = "11";
-    string b = "1";
-    if (argc>2){
-        a = argv[1];
-        b = argv[2];
-    }
-
-    cout << a << "+" << b << "=" << addBinary(a, b) << endl;
-    
-}
+};
