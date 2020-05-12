ce7d1085b5afa14266a39d70d1f8932d18579c79
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-05-12 18:54:32 +0900

    ボクセル

---
+++ algorithms/cpp/count-the-repetitions/pc2voxel.cpp
@@ -0,0 +1,8 @@
+//
+//  pc2voxel.cpp
+//  
+//
+//  Created by takayuki shinohara on 2020/05/12.
+//
+
+#include "pc2voxel.hpp"
---
+++ algorithms/cpp/count-the-repetitions/pc2voxel.hpp
@@ -0,0 +1,13 @@
+//
+//  pc2voxel.hpp
+//  
+//
+//  Created by takayuki shinohara on 2020/05/12.
+//
+
+#ifndef pc2voxel_hpp
+#define pc2voxel_hpp
+
+#include <stdio.h>
+
+#endif /* pc2voxel_hpp */
